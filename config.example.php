c22051d52712a5faf57cfd59b114ef8e016a4334
Raphaël Bastide <raphael.bastide@gmail.com>
2012-07-03 19:00:45 +0200

    repo mistake cleaned

--- config.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('GITHUB_USER', 'raphaelbastide');
-define('GITHUB_REPO', '1962');
-define('GITHUB_BRANCH', 'master');
-
-// Copy this password in the GitHub WebHook URL
-// e.g.: http://example.com/hook.php?password=your_password
-define('GITHUB_HOOK_PASSWORD', 'your_password');

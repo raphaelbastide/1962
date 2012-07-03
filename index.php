243107b7f600202227c26fbaea6e911cb8064245
Pierre Bertet <bonjour@pierrebertet.net>
2012-07-03 17:23:35 +0200

    Cache images and text files with GitHub WebHook
    This new version uses the GitHub WebHook to update the cache. The main
    advantage is that the loading time is not randomly distributed on the
    website users.

--- index.php
+++ index.php
@@ -1,90 +1,31 @@
-    <?php
-         $cache = '';
-        function get_repo_json($file, $plugin) {
-             global $cache;
-                 $current_time = time();
-                 $expire_time = 24 * 60 * 60;
-                 $file_time = @filemtime($file);
-                 if(file_exists($file) && ($current_time - $expire_time) < $file_time) { 
-                       $cache = false;
-                       return json_decode(file_get_contents($file),true); 
-                 } else {
-                       $cache = true;
-                       $json = array();
-                       $json['repo'] = json_decode(get('http://github.com/api/v2/json/repos/show/raphaelbastide/'.$plugin),true);
-                       $json['commit'] = json_decode(get('http://github.com/api/v2/json/commits/list/raphaelbastide/'.$plugin.'/master'),true);
-                       $json['readme'] = json_decode(get('http://github.com/api/v2/json/blob/show/raphaelbastide/'.$plugin.'/'.$json['commit']['commits'][0]['id'].'/README.md'),true);
-                       $json['piece'] = json_decode(get('http://github.com/api/v2/json/blob/show/raphaelbastide/'.$plugin.'/'.$json['commit']['commits'][0]['id'].'/PIECE'),true);
-                       $json['contributors'] = json_decode(get('http://github.com/api/v2/json/blob/show/raphaelbastide/'.$plugin.'/'.$json['commit']['commits'][0]['id'].'/CONTRIBUTORS'),true);
-                       file_put_contents($file,json_encode($json));
-                   return($json);
-                 }
-        }//end function
-    
-        /*
-        Description: gets the contents from a URL via cURL
-        @param $url (String) name of the url to get 
-        @return $data (String) content url 
-        */
-        function get($url){
-                $ch = curl_init();
-                curl_setopt($ch,CURLOPT_URL,$url);
-                curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-                curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,2);
-                $data = curl_exec($ch);
-                curl_close($ch);
-                if(empty($data)) {
-                       return 'Error retrieving. Try again. ';
-                } else {
-                       return $data;
-                } 
-        }//end function
-        if(isset($_GET['plugin'])) {
-             $plugin = trim($_GET['plugin']);  
-        } else {
-             $plugin = '1962';
-        }
-        $cache_path = 'cache-'.$plugin.'-github.txt';
-        $github_json = get_repo_json($cache_path,$plugin);
-        if($github_json) {
-           /* build content */
-           $name = $plugin; 
-           $desc= $github_json['repo']['repository']['description'];  
-           $readme= $github_json['readme']['blob']['data'];
-           $piece= $github_json['piece']['blob']['data'];
-           $contributors= $github_json['contributors']['blob']['data'];    
-        }//end if
-        
-        // IMG cache (thanks to Pierre Bertet)
-        
-        $cacheimg_file = __DIR__ . '/cache-img.json';
-        
-        function cacheimg_data($cacheimg_file) {
-            $data = file_get_contents('https://api.github.com/repos/raphaelbastide/1962/git/trees/master?recursive=1');
-            $data = json_decode($data);
-            $tree = $data->tree;
-            $tree = array_filter($tree, function($obj) {
-                return preg_match('/^media\/archive\/[^(HD)]+/', $obj->path);           });
-            file_put_contents('cache-img.json', json_encode($tree));
-        }
-        
-    ?>
+<?php
+
+require_once __DIR__ . '/config.php';
+require_once __DIR__ . '/lib.php';
+require_once __DIR__ . '/markdown.php';
+
+if (!file_exists(__DIR__. '/cache/README.md')) {
+  cache_all();
+}
+
+$readme = file_get_contents(__DIR__. '/cache/README.md');
+$piece = file_get_contents(__DIR__ . '/cache/PIECE');
+$contributors = file_get_contents(__DIR__ . '/cache/CONTRIBUTORS');
+?>
 <!doctype html>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title><?php echo$name; ?> — <?php echo$desc; ?> 
+    <title><?php echo $name; ?> — <?php echo $desc; ?>
 </title>
     <meta name="description" content="">
     <link rel="stylesheet" media="all" href="css/main.css">
     <link rel="stylesheet" type="text/css" media="print" href="css/print.css" />    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
     <link href='http://fonts.googleapis.com/css?family=Karla:400,400italic,700,700italic' rel='stylesheet' type='text/css'></head>
 <body>
-
-
     <?php
-        include('markdown.php');
-    ?> 
+
+    ?>
     <div id="content">
         <div id="colA">
             <ul id="menu">
@@ -106,82 +47,36 @@
             </div>
         </div>
         <div id="colB">
-        <?php        
-
-            function readimg_data($cacheimg_file, $load, $showLast) {
-                $data = file_get_contents('cache-img.json');
-                $tree = json_decode($data);
-                // Regex replacement
-                $patterns = array();
-                $patterns[0] = '/_/';
-                $patterns[1] = '/.jpg/';
-                $replacements = array();
-                $replacements[0] = '.';
-                $replacements[1] = '';
-                
-                // If we want to show all the images
-                if (!$showLast){
-                    foreach ($tree as $img) {
-                        $path = explode('/', $img->path);
-                        $filename = $path[count($path)-1];
-                        // If we want to load the image
-                        if ($load){
-                            echo '<div class="imgbox">';
-                            echo '<img src="https://github.com/raphaelbastide/1962/raw/master/'.$img->path.'" />';
-                            echo '<div class="caption">v'.preg_replace($patterns, $replacements, $filename).'</div>';
-                            echo '</div>';
-                        // If we don't want to load the image, src is placed in a 'data-src' attribute
-                        }else{
-                            echo '<div class="imgbox">';
-                            echo '<img data-src="https://github.com/raphaelbastide/1962/raw/master/'.$img->path.'" />';                    
-                            echo '<div class="caption">v'.preg_replace($patterns, $replacements, $filename).'</div>';
-                            echo '</div>';
-                        }
-                    }
-                // If we want to show the last image only
-                }else{
-                    $numItems = count($tree);
-                    $i = 0;
-                    foreach ($tree as $img) {
-                        $path = explode('/', $img->path);
-                        $filename = $path[count($path)-1];
-                        if($i == $numItems + 1) {
-                            if ($load){
-                                echo '<img src="https://github.com/raphaelbastide/1962/raw/master/'.$img->path.'" />';
-                                echo '<div class="caption">v'.preg_replace($patterns, $replacements, $filename).'</div>';
-                                //echo $commitDate;
-                            // Last image not loaded
-                            }else{
-                                echo '<img data-src="https://github.com/raphaelbastide/1962/raw/master/'.$img->path.'" />';                    
-                                echo '<div class="caption">v'.preg_replace($patterns, $replacements, $filename).'</div>';
-                            }
-                        }
-                        $i++;
-                    }
-                }
-                
-            }
-            
-            // In colB, we want to load the last image only
-            if (!file_exists($cacheimg_file)) {
-                cacheimg_data($cacheimg_file, true, true);
-            }
-            readimg_data($cacheimg_file, true, true);
-        ?>                        
+            <?php
+			function filename_to_tag($filename) {
+				return preg_replace(array('/_/', '/.jpg/'), array('.', ''), $filename);
+			}
+			$imgs = scandir(__DIR__ .'/img');
+			$imgs = array_values(array_filter($imgs, function($img) {
+				return $img !== '.' && $img !== '..';
+			}));
+			$last_img = $imgs[count($imgs)-1];
+			?>
+			<img src="img/<?php echo $last_img; ?>" alt="" />
+			<div class="caption">v<?php echo filename_to_tag($last_img); ?></div>
         </div>
     </div>
     <div class="page-break"></div>
     <div id="archives" >
         <div id="inner">
             <h2>Archives</h2>
-            <?
-                // In archives, we want to (not)load all the images, it will be loaded using JavaScript
-                readimg_data($cacheimg_file, false, false);
-            ?>                        
+				<?php
+				// In archives, we want to (not)load all the images, it will be loaded using JavaScript
+				foreach($imgs as $img): ?>
+				<div class="imgbox">
+					<img data-src="img/<?php echo $img; ?>" alt="" />
+					<div class="caption">v<?php echo filename_to_tag($img); ?></div>
+				</div>
+				<?php endforeach; ?>
         </div>
     </div>
     <footer>
-    This web page is synchronized each 24h with the <a href="https://github.com/raphaelbastide/1962">1962 GitHub repository</a>. The source of this website is <a href="https://github.com/raphaelbastide/Website-for-1962/">public and open source</a>.
+    This web page is synchronized with the <a href="https://github.com/<?php echo GITHUB_USER ?>/<?php echo GITHUB_REPO ?>">1962 GitHub repository</a>. The source of this website is <a href="https://github.com/raphaelbastide/Website-for-1962/">public and open source</a>.
     </footer>
     <script src="js/libs/jquery-1.7.1.min.js" type="text/javascript" charset="utf-8"></script>
     <script src="js/libs/jquery.history.js" type="text/javascript" charset="utf-8"></script>
@@ -190,12 +85,12 @@
       var _gaq = _gaq || [];
       _gaq.push(['_setAccount', 'UA-26267672-3']);
       _gaq.push(['_trackPageview']);
-    
+
       (function() {
         var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
         ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();    
+      })();
     </script>
 </body>
 </html>
